0bd273ed60cf4f1f0a67ce814dc3f239e5f8708f
Aishwarya SwamiNagure <swamiaishwarya24@gmail.com>
2025-04-08 14:38:13 +0530

    webtable

--- tests/webtable.spec.ts
+++ tests/webtable.spec.ts
@@ -5,11 +5,17 @@ test.only("webtable",async ({page}) => {
     const table= page.locator('//table[@name="BookTable"]');
     const headers=table.locator('th');
     const headercount=await headers.count();
+    console.log(headercount);
     for(let i=0;i<headercount;i++){
         console.log(await headers.nth(i).textContent())
     }
     const rows=table.locator('tr');
     const rowscount=await rows.count();
+
+    //fetch amit from table
+    const amit=table.locator('tbody tr:nth-child(2) td:nth-child(2)');
+    console.log(await amit.textContent());
+    await expect(amit).toHaveText('Amit');
   
     expect(headercount).toBe(4);
     expect(rowscount).toBe(7);
